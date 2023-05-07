5b6f71e7bf4b5bd8bcb6b2628d74164070ea72d9
DESKTOP-EFDVTUD\Alex <alexpaluch43@gmail.com>
2023-05-07 16:22:20 +0300

    add submit event

---
+++ app.js
@@ -0,0 +1,18 @@
+console.log("connected succesfully!")
+
+// lista de carti [[fata, spate], [fata, spate]]
+let cards_list = [];
+
+const form =  document.querySelector("form");
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const fd = new FormData(form);
+    let form_info = [];
+    for (item of fd){
+        form_info.push(item[1]);
+
+    }
+    cards_list.push(form_info); 
+    console.log(cards_list);
+})
